47f4a12cf62ac1cd96e54b2c8e3cf290ca6a1cfd
Georg Ledermann <georg@ledermann.dev>
2021-12-13 04:07:14 +0100

    Fix HoneyBadger setup

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,5 +1,5 @@
 import { Application } from '@hotwired/stimulus';
-import * as Honeybadger from '@honeybadger-io/js';
+import Honeybadger from '@honeybadger-io/js';
 import { metaContent } from '../utils/metaContent';
 
 const honeybadgerApiKey = metaContent('honeybadger-api-key');
