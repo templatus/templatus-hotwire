5a168399fae37c713717b06cad8a4a42ae7f2cb2
Georg Ledermann <georg@ledermann.dev>
2021-12-13 08:06:10 +0100

    Compress with Brotli

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,13 +1,16 @@
-const path = require('path');
+const esbuild = require('esbuild');
 const rails = require('esbuild-rails');
+const gzip = require('@luncheon/esbuild-plugin-gzip');
+const path = require('path');
 
-require("esbuild").build({
+esbuild.build({
   entryPoints: ["application.js"],
   bundle: true,
   sourcemap: true,
   minify: true,
+  write: false, // write must be false for gzip() to work
   outdir: path.join(process.cwd(), "app/assets/builds"),
   absWorkingDir: path.join(process.cwd(), "app/javascript"),
   watch: process.argv.includes("--watch"),
-  plugins: [rails()],
+  plugins: [gzip(), rails()],
 }).catch(() => process.exit(1));
