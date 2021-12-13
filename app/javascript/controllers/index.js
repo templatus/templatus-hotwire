28efabaf2e411e484b27bce4bc470c45ae32510c
Georg Ledermann <georg@ledermann.dev>
2021-12-13 08:30:15 +0100

    Cleanup Stimulus setup

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,35 @@
-import { application } from './application';
+import { Application } from '@hotwired/stimulus';
+import Honeybadger from '@honeybadger-io/js';
+import { metaContent } from '../utils/metaContent';
 
+const honeybadgerApiKey = metaContent('honeybadger-api-key');
+if (honeybadgerApiKey) {
+  const gitCommitVersion = metaContent('git-commit-version');
+
+  Honeybadger.configure({
+    apiKey: honeybadgerApiKey,
+    environment: 'production',
+    revision: gitCommitVersion,
+  });
+}
+
+// Start Stimulus application
+const application = Application.start();
+
+// Set up error handler
+if (honeybadgerApiKey) {
+  application.handleError = (error, message, detail) => {
+    console.warn(message, detail);
+    Honeybadger.notify(error);
+  };
+}
+
+// Configure Stimulus development experience
+application.warnings = true;
+application.debug = false;
+window.Stimulus = application;
+
+// Load and register Stimulus controller
 import controllers from './**/*_controller.js';
 controllers.forEach((controller) => {
   application.register(controller.name, controller.module.default);
