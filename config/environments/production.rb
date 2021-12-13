25fafe69b0aff8fc95899991d74d329fec6a1b37
Georg Ledermann <georg@ledermann.dev>
2021-12-13 03:44:40 +0100

    Compressing assets is made by cloudfront

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.asset_host = ENV.fetch('ASSET_HOST', nil).presence
 
+  config.assets.gzip = false
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
