67e357af51fa06b1e1082cdf5b6e4eda8a14edef
Georg Ledermann <georg@ledermann.dev>
2021-12-13 03:59:54 +0100

    Add assets config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,12 @@ Rails.application.configure do
     'Cache-Control' => 'public, s-maxage=31536000, max-age=31536000, immutable',
   }
 
+  # Compress CSS using a preprocessor.
+  # config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.asset_host = ENV.fetch('ASSET_HOST', nil).presence
 
