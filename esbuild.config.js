150781092bafef2a3f77c62e4c202caabc9e132f
Georg Ledermann <georg@ledermann.dev>
2021-12-13 15:05:09 +0100

    brotli does not work yet

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,15 +1,13 @@
 const esbuild = require('esbuild');
 const rails = require('esbuild-rails');
-const gzip = require('@luncheon/esbuild-plugin-gzip');
 const path = require('path');
 
 esbuild.build({
   entryPoints: ["application.js"],
   bundle: true,
   minify: true,
-  write: false, // write must be false for gzip() to work
   outdir: path.join(process.cwd(), "app/assets/builds"),
   absWorkingDir: path.join(process.cwd(), "app/javascript"),
   watch: process.argv.includes("--watch"),
-  plugins: [gzip(), rails()],
+  plugins: [rails()],
 }).catch(() => process.exit(1));
