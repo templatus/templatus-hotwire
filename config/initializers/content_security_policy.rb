31317f37adf9c458702f4003506f87ffe34d7810
Georg Ledermann <georg@ledermann.dev>
2022-01-05 07:07:40 +0100

    CSP: Fix Lookbook views by allowing inline styles in development

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -7,8 +7,7 @@
 Rails.application.configure do
   config.content_security_policy do |policy|
     if Rails.env.development?
-      policy.style_src :self,
-                       "'sha256-rql2tlBWA4Hb3HHbUfw797urk+ifBd6EAovoOUGt0oI='" # Turbo progress bar
+      policy.style_src :self, :unsafe_inline
       policy.script_src :self, :unsafe_eval, :unsafe_inline
       policy.connect_src :self,
                          "wss://#{ENV['APP_HOST']}",
