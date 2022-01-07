bea68a47a914e8013bbe916efbb46171d3f8b364
Georg Ledermann <georg@ledermann.dev>
2022-01-07 06:49:36 +0100

    Cleanup Rails 7.0.1 update

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -57,16 +57,13 @@ Rails.application.configure do
     #     "https://api.honeybadger.io/v1/browser/csp?api_key=#{Rails.configuration.x.honeybadger.api_key}&report_only=true",
     #   )
     # end
+
+    # Generate session nonces for permitted importmap and inline scripts
+    #   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+    #   config.content_security_policy_nonce_directives = %w(script-src)
+    #
+    #   # Report CSP violations to a specified URI. See:
+    #   # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
+    #   # config.content_security_policy_report_only = true
   end
 end
-
-# If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
-
-# Set the nonce only to specific directives
-# Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
-
-# Report CSP violations to a specified URI
-# For further information see the following documentation:
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
