caa9bea784f67fe2c80101583188809856aa3b5a
Georg Ledermann <georg@ledermann.dev>
2022-01-08 05:24:30 +0100

    :arrow_up: Update Ruby to 3.1.0

--- config/initializers/rack.rb
+++ config/initializers/rack.rb
@@ -19,8 +19,8 @@ module ActionDispatch
       @file_handler =
         FileHandler.new(
           path,
-          index: index,
-          headers: headers,
+          index:,
+          headers:,
           ##### Added "image/svg+xml"
           compressible_content_types: %r{\A(?:text/|application/javascript|image/svg\+xml)},
           #####
