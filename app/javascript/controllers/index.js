5cd9a768bb4e68af3f8eded446a636ecb4449808
Georg Ledermann <georg@ledermann.dev>
2022-08-01 04:23:34 +0200

    Drop outdated files

--- app/javascript/controllers/index.js
+++
@@ -1,7 +0,0 @@
-import controllers from './**/*_controller.js';
-
-export default function registerControllers(application) {
-  controllers.forEach((controller) => {
-    application.register(controller.name, controller.module.default);
-  });
-}
