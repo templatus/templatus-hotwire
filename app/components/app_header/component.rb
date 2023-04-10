bf29f37aada4472c40e31983872f395077f985fc
Georg Ledermann <georg@ledermann.dev>
2023-04-10 09:28:23 +0200

    Fix slim-lint warnings

--- app/components/app_header/component.rb
+++ app/components/app_header/component.rb
@@ -3,4 +3,8 @@ class AppHeader::Component < ViewComponent::Base
     @class = binding.local_variable_get(:class)
     super
   end
+
+  def tabs
+    [{ text: 'Home', href: root_path }, { text: 'About', href: about_path }]
+  end
 end
