a2b38b1f8a46c5c8c7e6b44ebd93a2b89ee06f93
Georg Ledermann <georg@ledermann.dev>
2023-04-25 06:53:30 +0200

    Use with_SLOT_NAME in v3

--- spec/components/tabs/component_spec.rb
+++ spec/components/tabs/component_spec.rb
@@ -2,11 +2,11 @@ describe Tabs::Component, type: :component do
   it 'renders links' do
     result =
       render_inline(described_class.new) do |c|
-        c.tabs [
-                 { text: 'One', href: '/1' },
-                 { text: 'Two', href: '/2' },
-                 { text: 'Three', href: '/3' },
-               ]
+        c.with_tabs [
+                      { text: 'One', href: '/1' },
+                      { text: 'Two', href: '/2' },
+                      { text: 'Three', href: '/3' },
+                    ]
       end
 
     expect(result.css('nav').to_html).to include('href="/1"')
