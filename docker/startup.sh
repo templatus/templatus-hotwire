8ec04b584be4bbc6ec69ecaa9790252377880ed4
Georg Ledermann <georg@ledermann.dev>
2023-08-11 19:30:18 +0200

    Add bash script formatter and reformat existing scripts

--- docker/startup.sh
+++ docker/startup.sh
@@ -7,16 +7,14 @@ echo "----------------"
 # Wait for Redis
 redis_host=$(echo "$REDIS_URL" | awk -F[/:] '{print $4}')
 redis_port=$(echo "$REDIS_URL" | awk -F[/:] '{print $5}')
-until nc -z -v -w30 "$redis_host" "$redis_port"
-do
+until nc -z -v -w30 "$redis_host" "$redis_port"; do
   echo "Waiting for Redis on $redis_host:$redis_port ..."
   sleep 1
 done
 echo "Redis is up and running!"
 
 # Wait for PostgreSQL
-until nc -z -v -w30 "$DB_HOST" 5432
-do
+until nc -z -v -w30 "$DB_HOST" 5432; do
   echo "Waiting for PostgreSQL on $DB_HOST:5432 ..."
   sleep 1
 done
