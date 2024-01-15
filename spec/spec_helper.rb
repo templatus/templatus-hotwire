dea8284f6d00bdd907d34c5747f7b6faa9e6651e
Georg Ledermann <georg@ledermann.dev>
2024-01-15 10:41:24 +0100

    RSpec: Allow running focused test only

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,14 +46,13 @@ RSpec.configure do |config|
 
   # The settings below are suggested to provide a good initial experience
   # with RSpec, but feel free to customize to your heart's content.
-=begin
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
   # is tagged with `:focus`, all examples get run. RSpec also provides
   # aliases for `it`, `describe`, and `context` that include `:focus`
   # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
   config.filter_run_when_matching :focus
-
+=begin
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
   # you configure your source control system to ignore this file.
