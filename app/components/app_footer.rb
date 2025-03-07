fa3834377f523fb257f39d99def2ba00408e7448
Georg Ledermann <georg@ledermann.dev>
2025-03-07 06:27:09 +0100

    Revert "Fix footer"
    This reverts commit 1477e4be0fc9a3fe23c18ead648db12d036bfa3c.

--- app/components/app_footer.rb
+++ app/components/app_footer.rb
@@ -7,7 +7,7 @@ class Components::AppFooter < Components::Base
   def view_template
     footer(
       class: [
-        'flex flex-wrap justify-between items-center px-4 space-y-2 text-sm text-white sm:px-6 lg:px-0',
+        'flex flex-wrap justify-between items-end px-4 space-y-2 text-sm text-white sm:px-6 lg:px-0',
         @class,
       ],
     ) do
@@ -60,8 +60,8 @@ class Components::AppFooter < Components::Base
   end
 
   def author
-    div(class: 'flex items-center w-full sm:w-auto') do
-      helpers.link_to(
+    div(class: 'flex items-end w-full sm:w-auto') do
+      link_to(
         'https://ledermann.dev',
         class: 'hover:underline',
         target: '_blank',
