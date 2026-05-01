8013e234bc2e9f97a1f21a0c31f806fdf16d595a
Georg Ledermann <georg@ledermann.dev>
2026-05-01 06:25:10 +0200

    Fix RuboCop warnings

--- spec/components/app_flash/component_spec.rb
+++ spec/components/app_flash/component_spec.rb
@@ -2,12 +2,12 @@ describe AppFlash::Component, type: :component do
   it 'renders notice' do
     result = render_inline(described_class.new(notice: 'All good!'))
 
-    expect(result).to have_content('All good!')
+    expect(result).to have_text('All good!')
   end
 
   it 'renders alert' do
     result = render_inline(described_class.new(alert: 'Something went wrong!'))
 
-    expect(result).to have_content('Something went wrong!')
+    expect(result).to have_text('Something went wrong!')
   end
 end
